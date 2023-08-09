144e63ed5b907d59622122ada457b8e031a53b9a
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-08-09 23:05:16 +0530

    jsx and components

--- 03-pizza-menu/final/src/index.js
+++ 03-pizza-menu/final/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
 const pizzaData = [
   {
@@ -47,131 +46,54 @@ const pizzaData = [
   },
 ];
 
-function App() {
+const App = () => {
   return (
-    <div className="container">
+    <div>
       <Header />
       <Menu />
       <Footer />
     </div>
   );
-}
+};
 
-function Header() {
-  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
-  const style = {};
-
+const Header = () => {
+  return <h1>Fast React Pizza Co.</h1>;
+};
+const Menu = () => {
   return (
-    <header className="header">
-      <h1 style={style}>Fast React Pizza Co.</h1>
-    </header>
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
   );
-}
-
-function Menu() {
-  const pizzas = pizzaData;
-  // const pizzas = [];
-  const numPizzas = pizzas.length;
-
-  return (
-    <main className="menu">
-      <h2>Our menu</h2>
-
-      {numPizzas > 0 ? (
-        <>
-          <p>
-            Authentic Italian cuisine. 6 creative dishes to choose from. All
-            from our stone oven, all organic, all delicious.
-          </p>
-
-          <ul className="pizzas">
-            {pizzas.map((pizza) => (
-              <Pizza pizzaObj={pizza} key={pizza.name} />
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>We're still working on our menu. Please come back later :)</p>
-      )}
-
-      {/* <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mushrooms"
-        price={12}
-        photoName="pizzas/funghi.jpg"
-      /> */}
-    </main>
-  );
-}
-
-function Pizza({ pizzaObj }) {
-  console.log(pizzaObj);
-
-  // if (pizzaObj.soldOut) return null;
-
-  return (
-    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
-      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
-      <div>
-        <h3>{pizzaObj.name}</h3>
-        <p>{pizzaObj.ingredients}</p>
-
-        {/* {pizzaObj.soldOut ? (
-          <span>SOLD OUT</span>
-        ) : (
-          <span>{pizzaObj.price}</span>
-        )} */}
-
-        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
-      </div>
-    </li>
-  );
-}
-
-function Footer() {
+};
+const Footer = () => {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
 
-  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
-  // else alert("Sorry we're closed");
-
-  // if (!isOpen) return <p>CLOSED</p>;
+  if (isOpen) alert(" We're currently open!");
+  else alert("sorry we're closed");
 
   return (
-    <footer className="footer">
-      {isOpen ? (
-        <Order closeHour={closeHour} openHour={openHour} />
-      ) : (
-        <p>
-          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
-        </p>
-      )}
-    </footer>
+    <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
   );
-
   // return React.createElement("footer", null, "We're currently open!");
-}
+};
 
-function Order({ closeHour, openHour }) {
+const Pizza = () => {
   return (
-    <div className="order">
-      <p>
-        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
-        online.
-      </p>
-      <button className="btn">Order</button>
+    <div>
+      <img src='pizzas/spinaci.jpg' alt='' />
+      <h2>Pizza Spinaci"</h2>
+      <p>"Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
-}
+};
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -181,5 +103,5 @@ root.render(
   </React.StrictMode>
 );
 
-// React before 18
+// before react 18
 // ReactDOM.render(<App />, document.getElementById("root"));
